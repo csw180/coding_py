7e4a04416b25b234bf56fdf33a44a8ff3c537691
csw180 <swkao@kakao.com>
2023-05-29 08:23:52 +0900

    boj1303 visited boolean으로 수정

--- boj_1303.py
+++ boj_1303.py
@@ -15,21 +15,25 @@ def dfs(start, matrix, visited) :
         r, c  = q.pop()
         if visited[r][c] :
             continue
-        visited[r][c] = 1
+        visited[r][c] = True
         cnt += 1
         for k in range(4) :
-            if matrix[r+pr[k]][c+pc[k]] == ch :
+            if (not visited[r+pr[k]][c+pc[k]]) and matrix[r+pr[k]][c+pc[k]] == ch :
                 q.append((r+pr[k],c+pc[k]))    
     return (ch,cnt)
 
 n,m = map(int,sys.stdin.readline().split(' '))
 matrix = [['' for _ in range(m+2)] for _ in range(n+2)]
-visited = [[0 for _ in range(m+2)] for _ in range(n+2)]
+visited = [[True for _ in range(m+2)] for _ in range(n+2)]
 for i in range(1,n+1) :
     inlist = list(sys.stdin.readline().rstrip())
     for j,v in enumerate(inlist,1) :
         matrix[i][j] = v
-
+        visited[i][j] = False
+# print('---------------------')
+# pprint.pprint(matrix)
+# pprint.pprint(visited)                
+# print('---------------------')
 
 score_w = 0
 score_b = 0
