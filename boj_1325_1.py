ef8c168d8d3890678d3e7495889c990c2bf4f377
csw180 <swkao@kakao.com>
2023-05-30 20:57:20 +0900

    boj1325 list 대신 dict 이용하는 버젼 _ 역시시간초과

--- boj_1325_1.py
+++ boj_1325_1.py
@@ -1,44 +1,51 @@
 # 백준 효율적인해킹(1325) https://www.acmicpc.net/problem/1325
-# 시간초과
+# list 대신 공간절약을 위하여 dict 를 이용하는 버젼으로 바꿔봄
+# 역시 시간초과
 
 import sys
 from collections import deque
 
 input  = sys.stdin.readline
 
-def bfs(start,trusts,n) :
+def bfs(start,coms) :
     q = deque()
     q.append(start)
     count = 0
-    visited = [False for _ in range(n+1)]
+    visited = dict()
     while q :
         c = q.popleft()
-        if visited[c] :
+        if  visited.get(c) :
             continue
         visited[c] = True
         count += 1
-        for next in trusts[c] :
-            if not visited[next] : 
+        if  not coms.get(c) :
+            continue
+        for next in coms[c] :
+            if  not visited.get(next) : 
                 q.append(next)
     return count-1       # 자기자신은 count 에서 제외 -1
 
 n, m = map(int,input().split())
-coms = [[] for _ in range(n+1)]
+coms = dict()
+
 for _ in range(m) :
     frm,to = map(int,input().split())
-    coms[to].append(frm)
+    if  not coms.get(to) :
+        coms[to] = [frm]
+    else :
+        coms[to].append(frm)
 # print(f'{coms=}')
 
 results =[]
 max_value = 0
-for i,com in enumerate(coms) :
+for k in  coms.keys() :
     count = 0
-    count = bfs(i,coms,n)
-    # print(f'{i=} {count=}')
+    count = bfs(k,coms)
+    # print(f'{k=} {count=}')
     if  count > max_value :
         max_value = count
-        results = [str(i)]
+        results = [str(k)]
     elif count == max_value :
-        results.append(str(i))
+        results.append(str(k))
 
 print(' '.join(results))
