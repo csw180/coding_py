f0069c4814c65c05dfc3f2d488067c42cc1e537b
csw180 <swkao@kakao.com>
2023-06-21 11:17:09 +0900

    프로그래머스17684 주석추가

--- pg17684.py
+++ pg17684.py
@@ -14,6 +14,11 @@ class WordDict :
         return f'temp_word={self._temp_word} index_list={self.index_list}'
 
     def append_char(self, char):
+        '''
+        추가하는 문자(char) 와 객체에 보관중인 temp_word 를 합쳐서 사전에 있는지 확인
+        사전에 있으면 temp_word 에 char 를 합체
+        사전에 없으면 temp_word 의 인덱스를 구하고 temp_word 를 비워고 char 를 temp_word 에 대입
+        '''
         if  self._temp_word+char in self._word_dict :
             self._temp_word += char
         else :
@@ -22,10 +27,13 @@ class WordDict :
             self._temp_word = char
 
     def flush(self):
+        '''
+        미처리한 temp_word 의 인덱스를 구한다.
+        '''
         self.index_list.append(self._word_dict.get(self._temp_word))
         self._temp_word = ''
 
-    def append(self, word):
+    def append(self, word):  # 사전에 단어를 등록한다.
         self.last_value += 1
         self._word_dict[word] = self.last_value
 
